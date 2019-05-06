91393436d3686689df40c31d11997c5ddcf7e53e
norahx3x <alnoor0416@gmail.com>
2019-05-06 12:57:47 +0300

    models and routs

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+require('dotenv').config()
+const app = express();
+const PORT = process.env.PORT;
+const mongoose= require('mongoose')
+//allow other to acssess
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+mongoose.connect(URL, {useNewUrlParser: true})
+.then(()=>{console.log("mongoose is running")},
+(error) =>{console.log(error)}
+)
+
+//listen on port
+app.listen(PORT, () => console.log(`active on ${PORT}`))
